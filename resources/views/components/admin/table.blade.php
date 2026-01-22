28f047994141165cf155aa33633f3f08e96273d4
Webmonte <dwebmonte@gmail.com>
2026-01-22 16:57:21 +0200

    fix: correct zebra stripes implementation in table component - use CSS nth-child instead of PHP loop to avoid ComponentSlot toArray() error

--- resources/views/components/admin/table.blade.php
+++ resources/views/components/admin/table.blade.php
@@ -6,7 +6,7 @@
 
 @php
 	$theadClass = $sticky ? 'sticky top-0 z-10' : '';
-	$tbodyClass = $zebra ? 'divide-y divide-slate-200' : 'divide-y divide-slate-200';
+	$tbodyClass = $zebra ? '[&>tr:nth-child(even)]:bg-slate-50/30' : '';
 @endphp
 
 <div {{ $attributes->merge(['class' => 'overflow-x-auto rounded-2xl border border-slate-200']) }}>
@@ -15,14 +15,8 @@
 			{{ $head ?? '' }}
 		</thead>
 
-		<tbody class="{{ $tbodyClass }}">
-			@php $rowIndex = 0; @endphp
-			@foreach($slot->toArray() as $row)
-				@php $rowIndex++; @endphp
-				<tr class="hover:bg-slate-50/70 {{ $zebra && $rowIndex % 2 === 0 ? 'bg-slate-50/30' : '' }}">
-					{{ $row }}
-				</tr>
-			@endforeach
+		<tbody class="divide-y divide-slate-200 {{ $tbodyClass }}">
+			{{ $slot }}
 		</tbody>
 	</table>
 </div>
