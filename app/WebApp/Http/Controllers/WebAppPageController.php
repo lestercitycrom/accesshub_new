cbacb6e314a0c5fe9fa0cc1c0e695997bbf8b488
Webmonte <dwebmonte@gmail.com>
2026-01-28 18:24:02 +0200

    fix

--- app/WebApp/Http/Controllers/WebAppPageController.php
+++ app/WebApp/Http/Controllers/WebAppPageController.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace App\WebApp\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Log;
 
 final class WebAppPageController
 {
 	public function __invoke(): View
 	{
-		return view('webapp.page');
+		try {
+			return view('webapp.page');
+		} catch (\Throwable $e) {
+			Log::error('WebAppPageController: Exception rendering view', [
+				'error' => $e->getMessage(),
+				'trace' => $e->getTraceAsString(),
+			]);
+			throw $e;
+		}
 	}
 }
